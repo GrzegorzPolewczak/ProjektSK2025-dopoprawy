58542f5ae17320944c40ef008c49d93b2268e129
Wdowik2115 <116074280+Wdowik2115@users.noreply.github.com>
2025-04-08 21:18:56 +0200

    Update ModelARX.cpp

--- ModelARX.cpp
+++ ModelARX.cpp
@@ -1,4 +1,4 @@
-#include "ModelARX.h"  
+#include "ModelARX.h"
 #include <vector>
 
 ModelARX::ModelARX(std::vector<double>& a_, std::vector<double>& b_, int opoznienie, double zaklocenie_ /*,double interwal*/)
@@ -7,32 +7,40 @@ ModelARX::ModelARX(std::vector<double>& a_, std::vector<double>& b_, int opoznie
     bufor_opoznienia = std::deque<double>(a.size(), 0.0);
 }
 
-ModelARX::ModelARX() {}
+ModelARX::ModelARX()
+    : a({0.0}), b({0.0}), opoznienie_k(1), zaklocenie(0.0)
+{
+    bufor_sterowania = std::deque<double>(b.size() + opoznienie_k, 0.0);
+    bufor_opoznienia = std::deque<double>(a.size(), 0.0);
+}
 
 double ModelARX::wykonajKrok(double wejscie) {
 
+    const size_t requiredSize = opoznienie_k + b.size();
+
     bufor_sterowania.push_front(wejscie);
 
-    const size_t requiredSize = opoznienie_k + b.size();
-    if (bufor_sterowania.size() < requiredSize) {
-        bufor_sterowania.resize(requiredSize, 0.0);  // Uzupełnij zerami jeśli za krótki
+    if (bufor_sterowania.size() > requiredSize) {
+        bufor_sterowania.resize(requiredSize);
     }
-    else if (bufor_sterowania.size() > requiredSize) {
-        bufor_sterowania.resize(requiredSize);       // Przytnij jeśli za długi
-    }
+
     double wyjscie = zaklocenie;
+
     for (size_t i = 0; i < b.size(); i++) {
         wyjscie += b[i] * bufor_sterowania[opoznienie_k + i];
     }
 
     for (size_t i = 0; i < a.size(); i++) {
-        wyjscie -= a[i] * bufor_opoznienia[i];
+        if (i < bufor_opoznienia.size()) {
+            wyjscie -= a[i] * bufor_opoznienia[i];
+        }
     }
 
     bufor_opoznienia.push_front(wyjscie);
     if (bufor_opoznienia.size() > a.size()) {
-        bufor_opoznienia.resize(bufor_opoznienia.size() - 1);
+        bufor_opoznienia.resize(a.size());
     }
+
     return wyjscie;
 }
 
